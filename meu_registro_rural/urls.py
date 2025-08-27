f0d689cd077fa0b66048a0e28f630aac3784857c
Augusto França <augustofranca0701@gmail.com>
2025-08-27 13:41:01 -0300

    fix: corrigir rota inicial para carregar painel_overview em /

--- meu_registro_rural/urls.py
+++ meu_registro_rural/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('painel_overview/', include('painel.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("painel.urls")),
 ]
